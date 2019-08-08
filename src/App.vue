ab7ad85258f08397bd74480a69d80a470d4564ee
Jeremy Kepf <jeremy1975@gmail.com>
2019-08-07 20:45:57 -0400

    2.8 3:59 finally found that v-for bug

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container">
+      <router-view />
+    </div> 
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'app'
 }
