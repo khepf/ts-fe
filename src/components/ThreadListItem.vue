d3ca7c7c2e8fdbad6ec362446efc009031a45597
Jeremy Kepf <jeremy1975@gmail.com>
2019-08-15 19:03:23 -0400

    refactored starter component

---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <router-link :to="{name: 'PageThreadShow', params: {id: thread['.key']}}">
+          {{thread.title}}
+        </router-link>
+      </p>
+      <p class="text-faded text-xsmall">
+        By <a href="#">{{user.name}}</a>, {{thread.publishedAt}}.
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">
+        {{repliesCount}} replies
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data'
+  export default {
+    props: {
+      thread: {
+        required: true,
+        type: Object
+      }
+    },
+
+    computed: {
+      repliesCount () {
+        return Object.keys(this.thread.posts).length - 1
+      },
+      user () {
+        return sourceData.users[this.thread.userId]
+      }
+    }
+  }
+</script>
---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-full" :class="$style.centered">
+    <h1 :class="$style.headingLarge">Not Found</h1>
+    <p :class="$style.textLarge">And I stillll haven't found what I'm looking for.</p>
+    <p :class="$style.textLarge"><router-link :to="{name: 'PageHome'}">Go Home and Try Again</router-link></p>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style module>
+.headingLarge {
+  font-size: 80px;
+}
+.textLarge {
+  font-size: 40px;
+  font-weight: 100;
+}
+div {
+  text-align: center;
+}
+
+</style>
