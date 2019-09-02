c4c63fa33eed91db739142fd7fabb8500f7522e6
Jeremy Kepf <jeremy1975@gmail.com>
2019-09-02 10:11:45 -0400

    start with 32

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -23,7 +23,7 @@
 </template>
 
 <script>
-
+import { countObjectProperties } from '@/utils'
 export default {
 
   props: {
@@ -37,7 +37,7 @@ export default {
       return this.$store.state.users[this.post.userId]
     },
     userPostsCount () {
-      return Object.keys(this.user.posts).length
+      return countObjectProperties(this.user.posts)
     }
   }
 }
--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -20,6 +20,7 @@
 </template>
 
 <script>
+  import { countObjectProperties } from '@/utils'
   export default {
 
     props: {
@@ -31,7 +32,7 @@
 
     computed: {
       repliesCount () {
-        return Object.keys(this.thread.posts).length - 1
+        return countObjectProperties(this.thread.posts) - 1
       },
       user () {
         return this.$store.state.users[this.thread.userId]
