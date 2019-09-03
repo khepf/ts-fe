e7f2be9fa90f18801d6b47589f71bb0797e7b45c
Jeremy Kepf <jeremy1975@gmail.com>
2019-09-03 19:34:20 -0400

    start with 34

--- src/store/index.js
+++ src/store/index.js
@@ -9,29 +9,43 @@ export default new Vuex.Store({
     ...sourceData,
     authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
   },
+
   getters: {
     authUser (state) {
       return state.users[state.authId]
     }
   },
+
   actions: {
     createPost (context, post) {
-      const postId = 'greatpost' + Math.random()
+      const postId = 'greatPost' + Math.random()
       post['.key'] = postId
-      context.commit('setPost', {post, postId})
+
+      context.commit('setPost', { post, postId })
       context.commit('appendPostToThread', { threadId: post.threadId, postId })
       context.commit('appendPostToUser', { userId: post.userId, postId })
+    },
+
+    updateUser ({ commit }, user) {
+      commit('setUser', { userId: user['.key'], user })
     }
   },
+
   mutations: {
-    setPost (state, {post, postId}) {
+    setPost (state, { post, postId }) {
       Vue.set(state.posts, postId, post)
     },
-    appendPostToThread (state, {postId, threadId}) {
+
+    setUser (state, { user, userId }) {
+      Vue.set(state.users, userId, user)
+    },
+
+    appendPostToThread (state, { postId, threadId }) {
       const thread = state.threads[threadId]
       Vue.set(thread.posts, postId, postId)
     },
-    appendPostToUser (state, {userId, postId}) {
+
+    appendPostToUser (state, { postId, userId }) {
       const user = state.users[userId]
       Vue.set(user.posts, postId, postId)
     }
