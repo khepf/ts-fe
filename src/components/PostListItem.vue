b5d97f2253f8dc8f947dbdf7203a3c26794543ef
Jeremy Kepf <jeremy1975@gmail.com>
2019-09-14 08:31:18 -0400

    start with 47

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -2,43 +2,65 @@
   <div class="post">
     <div class="user-info">
       <a href="#" class="user-name">{{user.name}}</a>
+
       <a href="#">
         <img class="avatar-large" :src="user.avatar" alt="">
       </a>
-      <p class="desktop-only text-small">{{userPostsCount}} posts</p>
 
+      <p class="desktop-only text-small">{{userPostsCount}} posts</p>
     </div>
 
     <div class="post-content">
-      <div>
-        {{post.text}}
+      <template v-if="!editing">
+        <div>
+          {{post.text}}
+        </div>
+        <a @click.prevent="editing = true" href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a>
+      </template>
+      <div v-else>
+        <PostEditor
+          :post="post"
+          @save="editing = false"
+          @cancel="editing = false"
+        />
       </div>
+
     </div>
 
-    <div 
-      class="post-date text-faded">
+    <div class="post-date text-faded">
       <AppDate :timestamp="post.publishedAt"/>
     </div>
   </div>
 </template>
 
 <script>
-import { countObjectProperties } from '@/utils'
-export default {
-
-  props: {
-    post: {
-      required: true,
-      type: Object
+    import {countObjectProperties} from '@/utils'
+    import PostEditor from './PostEditor'
+    export default {
+      props: {
+        post: {
+          required: true,
+          type: Object
+        }
+      },
+      components: {
+        PostEditor
+      },
+      data () {
+        return {
+          editing: false
+        }
+      },
+      computed: {
+        user () {
+          return this.$store.state.users[this.post.userId]
+        },
+        userPostsCount () {
+          return countObjectProperties(this.user.posts)
+        }
+      }
     }
-  },
-  computed: {
-    user () {
-      return this.$store.state.users[this.post.userId]
-    },
-    userPostsCount () {
-      return countObjectProperties(this.user.posts)
-    }
-  }
-}
 </script>
+
+<style scoped>
+</style>
